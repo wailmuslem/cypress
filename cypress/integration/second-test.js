7ff085d8a15c1156273ac2ffc5fac8da816e45c1
wailmuslem <99875309+wailmuslem@users.noreply.github.com>
2022-02-22 13:23:36 +0100

    Add files via upload

---
+++ cypress/integration/second-test.js
@@ -0,0 +1,24 @@
+context('Google homepage', () => {
+
+
+
+    beforeEach(() => {
+
+      cy.visit('https://www.google.se/')
+
+      cy.get('#L2AGLb > .QS5gu').click()
+
+      
+    })
+  
+    
+    it('Sveriges statsminister', function () {
+
+       
+             
+       
+           })
+       
+          
+        })
+       
